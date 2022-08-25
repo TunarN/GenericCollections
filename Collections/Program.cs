f0a3c7c9ef4d3a6e4235d2f548247987c84e42b9
TunarN <tunarzn@code.edu.az>
2022-08-25 17:57:44 +0400

    GenericCollections

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using Collections.models;
+using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLIst<int> L = new CustomLIst<int>();
+            L.Add(5);
+            L.Add(3);
+            L.Add(7);
+            L.Add(23);
+            L.Remove(21);
+            L.IndexOf(5);
+            L.Add(3);
+            L.Exist(4);
+            L.LastIndexOf(3);
+            L.Clear();
+            
+
+
+
+
+        }
+
+    }
+}
---
+++ Collections/models/CustomList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.models
+{
+    class CustomLIst<T>
+    {
+        T[] _arr;
+        int _counter;
+        public int Capacity { get; set; } = 3;
+        public int Count { get => _arr.Length - _counter; }
+        public CustomLIst()
+        {
+            _arr = new T[0];
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index < _arr.Length)
+                {
+                    return _arr[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index < _arr.Length)
+                {
+                    _arr[index] = value;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public void Add(T value)
+        {
+            if (_counter == 0)
+            {
+                Array.Resize(ref _arr, _arr.Length + Capacity);
+                _counter = Capacity;
+            }
+            _arr[Count] = value;
+            _counter--;
+
+        }
+        public void Clear()
+        {
+            _counter = 0;
+            _arr = new T[0];
+        }
+        public bool Exist(T item)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(item, _arr[i]))
+                    return true;
+            }
+
+            return false;
+        }
+        public void Remove(T item)
+        {
+            for (int i = IndexOf(item); i < _arr.Length - 1; i++)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+            Array.Resize(ref _arr, _arr.Length - 1);
+        }
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(_arr[i], item))
+                    return i;
+            } 
+
+            throw new KeyNotFoundException();
+        }
+        public int LastIndexOf(T item)
+        {
+            for (int i = _arr.Length - 1; i >= 0; i--)
+            {
+                if (object.Equals(_arr[i], item))
+                    return i;
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+}
